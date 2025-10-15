20f4471e29c37d0a0cfaf6d4a518826a68078b18
bruno150104 <brunotello1501@gmail.com>
2025-10-15 12:38:15 -0300

    Unidad 5 Completa

---
+++ Listas.py
@@ -0,0 +1,51 @@
+# ejercicio 1
+lista_multiplos_4 = list(range(4, 101, 4))
+print(lista_multiplos_4)
+
+# ejercicio 2
+gustos = ["pizza", "fútbol", "música", "viajar", "leer"]
+print("Penúltimo elemento:", gustos[-2])
+
+# ejercicio 3
+lista_vacia = []
+lista_vacia.append("sol")
+lista_vacia.append("luna")
+lista_vacia.append("estrella")
+print(lista_vacia)
+
+# ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+
+# ejercicio 5
+programa = ["inicio", "proceso", "fin"]
+print("Este programa crea una lista con tres elementos y la imprime.")
+
+# ejercicio 6
+numeros = list(range(10, 31, 5))
+print(numeros[:2])
+
+# ejercicio 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1:3] = ["camioneta", "coupe"]
+print(autos)
+
+# ejercicio 8
+dobles = []
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+
+# ejercicio 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+# ejercicio 10
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
